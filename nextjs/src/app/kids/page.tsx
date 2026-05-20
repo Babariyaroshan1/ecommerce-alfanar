ea3b662e64affc9d4172e311ca10f4adce6948b0
babariyaroshan1 <babariyaroshan123@gmail.com>
2026-05-20 09:38:29 +0530

    updated

--- nextjs/src/app/kids/page.tsx
+++ nextjs/src/app/kids/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 import KidsProductsClient from '@/components/KidsProductsClient';
 
 export const metadata: Metadata = {
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function KidsPage() {
-  return <KidsProductsClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <KidsProductsClient />
+    </Suspense>
+  );
 }
